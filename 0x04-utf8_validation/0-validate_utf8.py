6b278a2cb0ee57470bcc349b2d6579a9485da192
0xElectrifier-Ubuntu <samchijioke16@gmail.com>
2023-05-20 14:10:07 +0100

    First commit, UTF-8 validation

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""Alx Interview: UTF-8 Validation"""
+from typing import List
+
+
+def validUTF8(data: List) -> List[int]:
+    """
+    Method that determines if a given data set represents
+    a valid UTF-8 encoding.
+    Args:
+        data(list): A data set with values to be tested
+
+    Returns:
+        True if data is a valid UTF-8 encoding, else return False
+    """
+    if type(data) is not list:
+        return None
+    valid = True
+    for value in data:
+        if value < 0 or value > 127:
+            valid = False
+            break
+
+    return valid
